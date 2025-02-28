4a9ce1f9e1b71b204817c72ea3cac93688762cb5
Ravi Verma <raviverma@crownstack.com>
2025-02-28 14:01:38 +0530

    Worked on Archived workspace restore oage

--- resources/views/adminland/archivedworkspace.blade.php
+++ resources/views/adminland/archivedworkspace.blade.php
@@ -51,7 +51,7 @@
 								</td>
 								<td class="table-cell">
 									<div class="flex justify-end">
-										<button class="btn-inline-delete" data-modal-target="popup-modal-activate" data-modal-toggle="popup-modal-activate" href="#">Restore</button> 
+										<button onclick="restoreWorkspaceModal({{$workspace['id']}})" class="btn-inline-delete" data-modal-target="popup-modal-activate" data-modal-toggle="popup-modal-activate" href="#">Restore</button> 
 										<button class="btn-inline-delete ml-2" data-modal-target="popup-modal" data-modal-toggle="popup-modal" href="#">Delete</button>
 									</div>
 								</td>
@@ -88,6 +88,7 @@
 <div id="popup-modal-activate" data-modal-backdrop="static" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
 	<div class="relative p-4 w-full max-w-md max-h-full">
 		<div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+			<input type="hidden" id="workspace_id" value="">
 			<button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal-activate">
 				<svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
 					<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"></path>
@@ -102,10 +103,46 @@
 				<p class="mb-5 font-xs text-gray-500">Once this workspace is restored, its content will also be restored and users will be able
 					to see it.
 				</p> 
-				<button data-modal-hide="popup-modal-activate" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">Yes, I'm sure</button> 
+				<button onclick="restoreWorkspace()" data-modal-hide="popup-modal-activate" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">Yes, I'm sure</button> 
 				<button data-modal-hide="popup-modal-activate" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
 			</div>
 		</div>
 	</div>
 </div>
+
+
+<script>
+	function restoreWorkspaceModal(workspace_id) {
+		document.getElementById('workspace_id').value = workspace_id;
+	}
+
+	function restoreWorkspace() {
+		const workspace_id = document.getElementById('workspace_id').value;
+		const formData = new FormData();
+		formData.append('status', '1');
+
+		fetch('{{route("workspaces.updateWorkspaceStatus", ["workspace_id" => ":workspace_id"])}}'.replace(':workspace_id', workspace_id), {
+			method: 'post',
+			headers: {
+				'X-CSRF-TOKEN': '{{ csrf_token() }}',
+				'Accept': 'application/json'
+			},
+			body: formData
+		})
+		.then(response => response.json())
+		.then(data => {
+			if (data.success) {
+				alert(data.message);
+                window.location.reload();
+			} else {
+				alert(data.message);
+			}
+		})
+		.catch(error => {
+			alert("Something went wrong.");
+			console.error('Error:', error);
+		});
+	}
+	
+</script>
 @endsection
