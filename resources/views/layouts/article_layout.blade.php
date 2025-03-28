e79a0d43da21e8917ffa821c19e55a2845abd33f
Ravi Verma <raviverma@crownstack.com>
2025-03-28 15:56:19 +0530

    Worked on og tags for articles pages

--- resources/views/layouts/article_layout.blade.php
+++ resources/views/layouts/article_layout.blade.php
@@ -9,6 +9,11 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/datepicker.min.js"></script>
     <meta name="viewport" content="width=device-width" />
 
+    <meta property="og:url" content="https://quick-kb-dq2t.onrender.com/articles/quick-survey-67e67759639a8/billing-and-subscription-plans-67e677774f79d/primitive-obsession-a-common-anti-pattern-67e677ad4ed8b">
+    <meta property="og:type" content="article">
+    <meta property="og:title" content="Primitive Obsession: A Common Anti-Pattern">
+    <meta property="og:description" content="Signs and SymptomsReliance on primitives instead of small objects for specific tasks (e.g., using plain numbers for currency or strings for phone numb...">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <style>
